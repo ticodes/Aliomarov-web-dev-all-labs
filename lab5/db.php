b1d98afeb006f392f5b43b9d8aa587d31125c05e
ticodes <142982604+ticodes@users.noreply.github.com>
2023-12-09 13:52:03 +0300

    Create db.php

---
+++ lab5/db.php
@@ -0,0 +1,7 @@
+<?php 
+define('DB_HOST', 'localhost'); // Адрес
+define('DB_USER', 'root'); // Имя пользователя
+define('DB_PASSWORD', ''); // Пароль
+define('DB_NAME', 'lab5_data'); // Имя БД
+$mysql = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME)
+?>
